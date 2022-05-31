dc8096e0e2ab07be8e1a0f0f5244dd0393dbde0f
Balamurugan011 <balamuruganiyappanbm@gmail.com>
2022-05-31 18:18:46 +0530

    Second Commit

--- src/org/test/Project.java
+++ src/org/test/Project.java
@@ -2,9 +2,12 @@ package org.test;
 
 public class Project {
 	private void test1() {
-    System.out.println("Test1");
+		System.out.println("Test1");
 	}
-    private void test2() {
-    System.out.println("test2");
+	private void test2() {
+		System.out.println("test2");
+	}
+	private void test4() {
+		System.out.println("test4");
 	}
 }
